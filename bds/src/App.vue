9f5e54d18750de91f6a34285d20a808d6c3e5410
Shichao <92007182+Shigakki@users.noreply.github.com>
2022-09-28 20:21:40 +0800

    vue-add
    前端element-UI测试

--- bds/src/App.vue
+++ bds/src/App.vue
@@ -1,6 +1,19 @@
 <template>
   <div id="app">
-    <router-view/>
+    <!-- 测试elementUI -->
+    <el-row>
+      <el-button>默认按钮</el-button>
+      <el-button type="primary">主要按钮</el-button>
+      <el-button type="success">成功按钮</el-button>
+      <el-button type="info">信息按钮</el-button>
+      <el-button type="warning">警告按钮</el-button>
+      <el-button type="danger">危险按钮</el-button>
+    </el-row>
+    <div id="nav">
+      <router-link to="/">Home</router-link>|
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
   </div>
 </template>
 
